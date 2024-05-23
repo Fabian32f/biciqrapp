fc3cf81060a2fb56a9339d5459cfd8cfa7bdf4c6
Fabia <221k0022@itscarrillopuerto.edu.mx>
2024-05-23 00:16:14 -0500

    funcionamiento de el lector de qr

--- lib/main.dart
+++ lib/main.dart
@@ -38,7 +38,30 @@ class MyApp extends StatelessWidget {
         '/profile': (context) => ProfileScreen(),
         '/scanner': (context) => ScannerScreen(),
         '/print_qr': (context) => PrintQRScreen(),
+        '/scanner': (context) => ScannerScreen(),
       },
     );
   }
 }
+
+
+
+/*
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Screen'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pushNamed(context, '/scanner');
+          },
+          child: Text('Escanear QR'),
+        ),
+      ),
+    );
+  }
+}*/
--- lib/screens/scanner_screen.dart
+++ lib/screens/scanner_screen.dart
@@ -1,33 +1,77 @@
 import 'package:flutter/material.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
+import 'package:flutter/foundation.dart';
 
-class ScannerScreen extends StatelessWidget {
+class ScannerScreen extends StatefulWidget {
+  @override
+  _ScannerScreenState createState() => _ScannerScreenState();
+}
+
+class _ScannerScreenState extends State<ScannerScreen> {
+  final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
+  Barcode? result;
+  QRViewController? controller;
+
+  @override
+  void reassemble() {
+    super.reassemble();
+    if (defaultTargetPlatform == TargetPlatform.android) {
+      controller!.pauseCamera();
+    } else if (defaultTargetPlatform == TargetPlatform.iOS) {
+      controller!.resumeCamera();
+    }
+  }
+
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-          'Escáner',
-          style: TextStyle(
-            fontSize: 24.0,
-            fontWeight: FontWeight.bold,
-            shadows: [
-              Shadow(
-                blurRadius: 5.0,
-                color: Colors.blue,
-                offset: Offset(2.0, 2.0),
-              ),
-            ],
-          ),
-        ),
-        backgroundColor: Colors.grey[200],
-        elevation: 0, // Remover la sombra del AppBar
+        title: Text('Escanear Código QR'),
       ),
-      body: Center(
-        child: Text(
-          'Contenido del escáner aquí...',
-          style: TextStyle(fontSize: 20),
-        ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            flex: 5,
+            child: QRView(
+              key: qrKey,
+              onQRViewCreated: _onQRViewCreated,
+              overlay: QrScannerOverlayShape(
+                borderColor: Colors.red,
+                borderRadius: 10,
+                borderLength: 30,
+                borderWidth: 10,
+                cutOutSize: 300,
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Center(
+              child: (result != null)
+                  ? Text('Código escaneado: ${result!.code}')
+                  : Text('Escanea un código QR'),
+            ),
+          )
+        ],
       ),
     );
+  }
+
+  void _onQRViewCreated(QRViewController controller) {
+    this.controller = controller;
+    controller.scannedDataStream.listen((scanData) {
+      setState(() {
+        result = scanData;
+      });
+      // Aquí puedes manejar el resultado del escaneo, por ejemplo, navegar a otra pantalla
+      // o realizar una búsqueda en tu base de datos
+      // Navigator.pushNamed(context, '/resultado', arguments: result!.code);
+    });
   }
 }
